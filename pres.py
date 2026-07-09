998bf1b3ae35b02a97180afd2e2b81834c6a1ee9
HuskMusker <sgrushny@gmail.com>
2026-07-10 04:31:53 +0500

    Update pres.py

--- pres.py
+++ pres.py
@@ -10,7 +10,7 @@ import streamlit.components.v1 as components
 # ------------------------------
 st.set_page_config(
     page_title="Цифровой наставник",
-    page_icon="⚖️",
+    page_icon="",
     layout="wide",
     initial_sidebar_state="expanded"
 )
