5eed27dc5ee45c7f585a11be188d483fbd601534
HuskMusker <sgrushny@gmail.com>
2026-07-11 15:13:25 +0500

    Update pres.py

--- pres.py
+++ pres.py
@@ -22,7 +22,7 @@ st.markdown(
     """
 <style>
 :root {
-    --bg-primary: #1C1A3B;
+    --bg-primary: #1A1836;
     --bg-card: rgba(255, 255, 255, 0.05);
     --bg-card-hover: rgba(255, 255, 255, 0.08);
     --border-card: rgba(255, 255, 255, 0.06);
