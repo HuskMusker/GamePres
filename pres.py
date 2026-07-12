c482aac6c5eefdf7609ce8e7a11cf384b03defe9
HuskMusker <sgrushny@gmail.com>
2026-07-12 18:47:26 +0500

    Update pres.py

--- pres.py
+++ pres.py
@@ -52,8 +52,7 @@ if base64_img:
         position: fixed;
         top: 0; left: 0;
         width: 100%; height: 100%;
-        background-color: rgba(17, 14, 31, 0.94);
-         filter: blur(8px);
+        background-color: rgba(17, 14, 31, 0.97);
         z-index: 0;                         /* выше body-фона, но ниже контента */
         pointer-events: none;
     }}
