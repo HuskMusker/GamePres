4bb5c5a6c43fa1a02070fa4c7a1ff9a69908c976
HuskMusker <sgrushny@gmail.com>
2026-07-12 16:44:41 +0500

    Update pres.py

--- pres.py
+++ pres.py
@@ -53,8 +53,7 @@ st.markdown(
 }
 
 .stApp {
-    background: linear-gradient(rgba(17,14,31,0.85), rgba(17,14,31,0.85)), 
-    url('bggg.png') center/cover no-repeat fixed;
+    background: url('bg.png') center/cover no-repeat fixed;
     color: var(--text-primary);
     font-family: var(--font-family);
     line-height: 1.6;
